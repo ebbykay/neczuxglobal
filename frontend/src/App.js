2e4b08ec3d3c86730064d161729a6e12f23191ec
Ebenezer Kwakye <ebenezerkwakye486@gmail.com>
2024-08-27 16:07:22 +0000

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Neczux Global Group</h1>
+        <p>Empowering individuals and businesses with top-tier services.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
